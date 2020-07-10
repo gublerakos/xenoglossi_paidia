3b5d0188ba522c538e3b8dcf8ee926af4e2f397a
gublerakos <mariaapntz@gmail.com>
2020-07-10 22:15:19 +0300

    front page desktop only

---
+++ js/header.js
@@ -0,0 +1,22 @@
+document.write("<div class=\"header\">");
+document.write("<a href=\"..\/index.html\" class=\"home\">");
+document.write("<h1>Αρχική<\/h1>");
+document.write("<\/a>");
+
+document.write("<a href=\"..\/lang\/languages.html\" class=\"languages\">");
+document.write("<h1>Γλώσσες<\/h1>");
+document.write("<\/a>");
+
+document.write("<a href=\"..\/crew\/crew.html\" class=\"crew\">");
+document.write("<h1>Καθηγητές<\/h1>");
+document.write("<\/a>");
+
+document.write("<a href=\"..\/offers\/offers.html\" class=\"offers\">");
+document.write("<h1>Προσφορές<\/h1>");
+document.write("<\/a>");
+
+document.write("<a href=\"..\/contact\/contact.html\" class=\"contact\">");
+document.write("<h1>Επικοινωνία<\/h1>");
+document.write("<\/a>");
+
+document.write("<\/div>");
