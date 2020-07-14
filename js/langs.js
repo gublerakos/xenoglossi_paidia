ce7b7eb23fd24637dcfe741f3ea06a020356d4f3
gublerakos <mariaapntz@gmail.com>
2020-07-15 00:50:54 +0300

    tried dropdown

---
+++ js/langs.js
@@ -0,0 +1,6 @@
+document.write("			if (x.style.display === \"grid\") {");
+document.write("				x.style.display = \"none\";");
+document.write("			}");
+document.write("			else {");
+document.write("				x.style.display = \"grid\";");
+document.write("			}");
